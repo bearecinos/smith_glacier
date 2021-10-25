eab52a9450a6a7162494dd519d440fbfd1e32757
bearecinos <beatriz.recinos@ed.ac.uk>
2021-10-25 15:34:49 +0100

    set paths .sh for faster navigation in bow

---
+++ setpaths.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export MAIN_PATH=/home/brecinos/smith_glacier
+export INPUT_DIR=$MAIN_PATH/input
+export PREPRO_STAGES=$MAIN_PATH/scripts/prepro_stages
+export RUN_CONFIG_DIR=$MAIN_PATH/scripts/run_stages
+export OUTPUT_DIR=$MAIN_PATH/output
+
+# Paths to fenics_ice
+export RUN_DIR=$FENICS_ICE_BASE_DIR/runs/
+
+
+echo 'Input data directory is '$INPUT_DIR
+echo 'Runs configuration directory is '$RUN_CONFIG_DIR
+echo 'Prepro files run routines are in '$PREPRO_STAGES
+echo 'All output directory is '$OUTPUT_DIR
+echo 'Workflow run scripts '$RUN_DIR
+

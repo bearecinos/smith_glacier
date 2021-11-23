57e4dc3f6dab8f80ea12878dabdb868f305b6e15
bearecinos <beatriz.recinos@ed.ac.uk>
2021-11-23 16:23:23 +0000

    added plot routines, need to be improve later

---
+++ scripts/plot_stages/plot_input_data.py
@@ -0,0 +1,205 @@
+"""
+Plots mesh and Gridded data used as input
+for Fenics_ice
+
+- Reads the input mesh
+- Reads input data
+- Plots everything (well the most interesting data)
+ in a multiple grid
+
+@authors: Fenics_ice contributors
+"""
+import os
+import salem
+import h5py
+import numpy as np
+from fenics import *
+from fenics_ice import config, inout
+from pathlib import Path
+from fenics_ice import mesh as fice_mesh
+
+#Plotting imports
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+from matplotlib.offsetbox import AnchoredText
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import seaborn as sns
+import matplotlib.tri as tri
+
+
+rcParams['axes.labelsize'] = 18
+rcParams['xtick.labelsize'] = 18
+rcParams['ytick.labelsize'] = 18
+
+color = sns.color_palette()
+cmap_topo = salem.get_cmap('topo')
+cmap_thick = plt.cm.get_cmap('YlGnBu')
+cmap_glen=plt.get_cmap('RdBu_r')
+
+#Load main repository path
+MAIN_PATH = os.path.expanduser('~/scratch/smith_glacier/')
+sys.path.append(MAIN_PATH)
+
+# Paths to data
+plot_path = os.path.join(MAIN_PATH, 'plots/')
+if not os.path.exists(plot_path):
+    os.makedirs(plot_path)
+
+run_files = os.path.join(MAIN_PATH, 'scripts/run_stages/run_inversion')
+toml = os.path.join(run_files, 'smith.toml')
+
+params = config.ConfigParser(toml, top_dir=Path(MAIN_PATH))
+
+#Reading mesh
+mesh_in = fice_mesh.get_mesh(params)
+
+# Constructing mesh functions from mesh
+Q = FunctionSpace(mesh_in, 'Lagrange',1)
+Qh = FunctionSpace(mesh_in, 'Lagrange',3)
+M = FunctionSpace(mesh_in, 'DG', 0)
+
+Qp = Q
+V = VectorFunctionSpace(mesh_in, 'Lagrange', 1, dim=2)
+
+x = mesh_in.coordinates()[:,0]
+y = mesh_in.coordinates()[:,1]
+t = mesh_in.cells()
+
+trim = tri.Triangulation(x, y, t)
+
+# Read bed machine data
+bedmachine = os.path.join(params.io.input_dir, params.io.bed_data_file)
+bedmachine_smith = h5py.File(bedmachine, 'r')
+
+bed = bedmachine_smith['bed'][:]
+thick = bedmachine_smith['thick'][:]
+surf_ice = bedmachine_smith['surf'][:]
+x_bm = bedmachine_smith['x'][:]
+y_bm = bedmachine_smith['y'][:]
+
+xgbm, ygbm = np.meshgrid(x_bm, y_bm)
+
+#Read velocity for inversion
+out = inout.read_vel_obs(params, model=None)
+uv_obs_pts = out[0]
+u_obs = out[1]
+v_obs = out[2]
+
+u_std = out[3]
+v_std = out[4]
+x_vel, y_vel = np.split(uv_obs_pts, [-1], axis=1)
+vel_obs = np.sqrt(u_obs**2 + v_obs**2)
+
+xgvel, ygvel = np.meshgrid(np.unique(x_vel), np.unique(y_vel))
+vel_obs_g = np.flipud(vel_obs.reshape(xgvel.shape))
+
+# Read bglen
+b_glen_file = os.path.join(params.io.input_dir, params.io.bglen_data_file)
+b_glen = h5py.File(b_glen_file, 'r')
+
+bglen = b_glen['bglen'][:]
+x_bg, y_bg = np.meshgrid(b_glen['x'][:], b_glen['y'][:])
+
+# Now plotting
+# Now plotting
+g = 1.2
+
+tick_options = {'axis':'both','which':'both','bottom':False,
+    'top':False,'left':False,'right':False,'labelleft':False, 'labelbottom':False}
+
+fig1 = plt.figure(figsize=(10*g, 14*g))#, constrained_layout=True)
+spec = gridspec.GridSpec(3, 2, wspace=0.01, hspace=0.2)
+
+ax0 = plt.subplot(spec[0])
+ax0.set_aspect('equal')
+ax0.tick_params(**tick_options)
+ax0.set_xlim(min(x), max(x))
+ax0.set_ylim(min(y), max(y))
+ax0.triplot(x, y, trim.triangles, '-', color='k', lw=0.2)
+at = AnchoredText('a', prop=dict(size=18), frameon=True, loc='upper left')
+ax0.add_artist(at)
+
+ax1 = plt.subplot(spec[1])
+ax1.set_aspect('equal')
+ax1.tick_params(**tick_options)
+ax1.set_xlim(min(x), max(x))
+ax1.set_ylim(min(y), max(y))
+minv = -3000
+maxv = 3000
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+divider = make_axes_locatable(ax1)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+c = ax1.contourf(xgbm, ygbm, bed, levels = levels, cmap=cmap_topo)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('bed altitude [m above s.l.]')
+at = AnchoredText('b', prop=dict(size=18), frameon=True, loc='upper left')
+ax1.add_artist(at)
+
+ax2 = plt.subplot(spec[2])
+ax2.set_aspect('equal')
+ax2.tick_params(**tick_options)
+ax2.set_xlim(min(x), max(x))
+ax2.set_ylim(min(y), max(y))
+minv = 1
+maxv = 3000
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+divider = make_axes_locatable(ax2)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+c = ax2.contourf(xgbm, ygbm, thick, levels = levels, cmap=cmap_thick)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('Ice thickness [m]')
+at = AnchoredText('c', prop=dict(size=18), frameon=True, loc='upper left')
+ax2.add_artist(at)
+
+ax3 = plt.subplot(spec[3])
+ax3.set_aspect('equal')
+ax3.tick_params(**tick_options)
+ax3.set_xlim(min(x), max(x))
+ax3.set_ylim(min(y), max(y))
+minv = 0
+maxv = 3000
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+divider = make_axes_locatable(ax3)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+c = ax3.contourf(xgbm, ygbm, surf_ice, levels = levels, cmap=cmap_topo)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('Ice surface elevation [m above s.l.]')
+at = AnchoredText('d', prop=dict(size=18), frameon=True, loc='upper left')
+ax3.add_artist(at)
+
+ax4 = plt.subplot(spec[4])
+ax4.set_aspect('equal')
+ax4.tick_params(**tick_options)
+divider = make_axes_locatable(ax4)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+minv = 0
+maxv = 2000
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+c = ax4.contourf(xgvel, ygvel, vel_obs_g, levels = levels, cmap='viridis')
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('ice surface velocity [$m^{-1}$ yr]')
+at = AnchoredText('e', prop=dict(size=18), frameon=True, loc='upper left')
+ax4.add_artist(at)
+
+ax5 = plt.subplot(spec[5])
+ax5.set_aspect('equal')
+ax5.tick_params(**tick_options)
+divider = make_axes_locatable(ax5)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+minv = np.min(bglen)
+maxv = np.max(bglen)
+ticks = np.linspace(minv,maxv,3)
+levels = np.linspace(minv,maxv,200)
+c = ax5.contourf(x_bg, y_bg, bglen, levels = levels, cmap=cmap_glen)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('A creep parameter [Pa $yr^{1/3}$]')
+at = AnchoredText('f', prop=dict(size=18), frameon=True, loc='upper left')
+ax5.add_artist(at)
+
+plt.tight_layout()
+plt.savefig(os.path.join(plot_path, 'input_data.pdf'), bbox_inches='tight')
---
+++ scripts/plot_stages/plot_inversion.py
@@ -0,0 +1,186 @@
+"""
+Plot inversion output
+
+- Reads the input mesh
+- Reads output data (this can be any of the output
+ from the inversion if it is stored as .xml)
+- Plots things in a multiplot grid
+
+@authors: Fenics_ice contributors
+"""
+import sys
+import os
+from fenics import *
+from fenics_ice import config
+from fenics_ice import mesh as fice_mesh
+from pathlib import Path
+import seaborn as sns
+import numpy as np
+
+# Matplotlib imports
+from matplotlib import rcParams
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import AnchoredText
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib.tri as tri
+
+# Plot settings
+rcParams['axes.labelsize'] = 18
+rcParams['xtick.labelsize'] = 18
+rcParams['ytick.labelsize'] = 18
+color = sns.color_palette()
+cmap_vel= plt.get_cmap('viridis')
+cmap_params = plt.get_cmap('RdBu_r')
+tick_options = {'axis':'both','which':'both','bottom':False,
+    'top':False,'left':False,'right':False,'labelleft':False, 'labelbottom':False}
+
+
+# Define main repository path
+MAIN_PATH = os.path.expanduser('~/scratch/smith_glacier/')
+sys.path.append(MAIN_PATH)
+
+# Paths to data
+plot_path = os.path.join(MAIN_PATH, 'plots/')
+if not os.path.exists(plot_path):
+    os.makedirs(plot_path)
+
+# Define directory where the .toml file is stored and
+# where we have our run output directory
+run_files = os.path.join(MAIN_PATH, 'scripts/run_stages/run_inversion')
+toml = os.path.join(run_files, 'smith.toml')
+output_dir = os.path.join(MAIN_PATH, 'output/03_run_inv')
+
+# Read in model run parameters
+params = config.ConfigParser(toml, top_dir=Path(MAIN_PATH))
+
+#Read and get mesh information
+mesh_in = fice_mesh.get_mesh(params)
+
+print('We are using this velocity data', params.config_dict['obs'])
+
+# Compute the function spaces from the Mesh
+Q = FunctionSpace(mesh_in, 'Lagrange',1)
+Qh = FunctionSpace(mesh_in, 'Lagrange',3)
+M = FunctionSpace(mesh_in, 'DG', 0)
+
+if not params.mesh.periodic_bc:
+   Qp = Q
+   V = VectorFunctionSpace(mesh_in,'Lagrange', 1, dim=2)
+else:
+    Qp = fice_mesh.get_periodic_space(params, mesh_in, dim=1)
+    V =  fice_mesh.get_periodic_space(params, mesh_in, dim=2)
+
+# Read output data to plot
+U_file = os.path.join(params.io.output_dir, params.io.run_name+'_U.xml')
+uv_comp_file = os.path.join(params.io.output_dir, params.io.run_name+'_uv_obs.xml')
+alpha_init_file = os.path.join(params.io.output_dir, params.io.run_name+'_alpha_init_guess.xml')
+alpha_file = os.path.join(params.io.output_dir, params.io.run_name+'_alpha.xml')
+
+# Define function spaces
+U = Function(V, U_file)
+alpha = Function(Qp, alpha_file)
+alpha_init = Function(Qp, alpha_init_file)
+uv_comp = Function(M, uv_comp_file)
+
+#Sort out velocity components
+u, v = U.split()
+uv = project(sqrt(u*u + v*v), Q)
+uv_diff = project(uv_comp - uv, Q)
+uv_comp_proj = project(uv_comp, Q)
+
+# 1. The inverted value of B2; It is explicitly assumed that B2 = alpha**2
+B2 = project(alpha*alpha, M)
+alpha_ini = project(alpha_init, M)
+
+# Get mesh triangulation
+x = mesh_in.coordinates()[:,0]
+y = mesh_in.coordinates()[:,1]
+t = mesh_in.cells()
+trim = tri.Triangulation(x, y, t)
+
+# Compute vertex values for each parameter function
+# in the mesh
+v = uv.compute_vertex_values(mesh_in)
+v_comp = uv_comp_proj.compute_vertex_values(mesh_in)
+
+v_alpha = B2.compute_vertex_values(mesh_in)
+v_alphaini = alpha_ini.compute_vertex_values(mesh_in)
+
+# Now plotting
+g = 1.5
+fig1 = plt.figure(figsize=(10*g, 10*g))#, constrained_layout=True)
+spec = gridspec.GridSpec(2, 2, hspace=0.01)#, wspace=0.05, hspace=0.05)
+
+
+ax0 = plt.subplot(spec[0])
+ax0.set_aspect('equal')
+ax0.tick_params(**tick_options)
+minv = 0
+maxv = 2000
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+divider = make_axes_locatable(ax0)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+c = ax0.tricontourf(x, y, t, v, levels = levels, cmap=cmap_vel)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('[$m^{-1}$ yr]')
+ax0.set_title("Model ice velocity (U)", fontsize=18)
+at = AnchoredText('a', prop=dict(size=18), frameon=True, loc='upper left')
+ax0.add_artist(at)
+
+
+ax1 = plt.subplot(spec[1])
+ax1.set_aspect('equal')
+ax1.tick_params(**tick_options)
+divider = make_axes_locatable(ax1)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+c = ax1.tricontourf(x, y, t, v_comp, levels = levels, cmap=cmap_vel)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('[$m^{-1}$ yr]')
+ax1.set_title("Velocity observations (MEaSUREs v2.0)", fontsize=18)
+at = AnchoredText('b', prop=dict(size=18), frameon=True, loc='upper left')
+ax1.add_artist(at)
+
+
+ax2 = plt.subplot(spec[2])
+ax2.set_aspect('equal')
+ax2.tick_params(**tick_options)
+divider = make_axes_locatable(ax2)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+ax2.triplot(x, y, trim.triangles, '-', color='grey', lw=0.2, alpha=0.5)
+minv = -70
+maxv = 70
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+c = ax2.tricontourf(x, y, t, v_alphaini, levels = levels, cmap=cmap_params)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('alpha initial guess')
+ax2.set_xlim(min(x), max(x))
+ax2.set_ylim(min(y), max(y))
+at = AnchoredText('c', prop=dict(size=18), frameon=True, loc='upper left')
+ax2.set_title("Alpha first guess", fontsize=18)
+ax2.add_artist(at)
+
+ax3 = plt.subplot(spec[3])
+ax3.set_aspect('equal')
+ax3.tick_params(**tick_options)
+divider = make_axes_locatable(ax3)
+cax = divider.append_axes("bottom", size="5%", pad=0.1)
+ax3.triplot(x, y, trim.triangles, '-', color='grey', lw=0.2, alpha=0.5)
+minv = 0
+maxv = np.max(v_alpha)
+levels = np.linspace(minv,maxv,200)
+ticks = np.linspace(minv,maxv,3)
+c = ax3.tricontourf(x, y, t, v_alpha, levels = levels, cmap=cmap_params)
+cbar = plt.colorbar(c, cax=cax, ticks=ticks, orientation="horizontal")
+cbar.ax.set_xlabel('$B^{2}$')
+ax3.set_xlim(min(x), max(x))
+ax3.set_ylim(min(y), max(y))
+at = AnchoredText('d', prop=dict(size=18), frameon=True, loc='upper left')
+ax3.set_title('inverse of $B^{2}$', fontsize=18)
+ax3.add_artist(at)
+
+plt.tight_layout()
+plt.savefig(os.path.join(plot_path, 'inversion_output.png'), bbox_inches='tight')
+

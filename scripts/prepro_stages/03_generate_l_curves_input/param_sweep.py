f0207154ea114b582aa889031daba41406e13755
bearecinos <beatriz.recinos@ed.ac.uk>
2021-11-30 12:46:39 +0000

    lcurve exp first commit

---
+++ scripts/prepro_stages/03_generate_l_curves_input/param_sweep.py
@@ -0,0 +1,147 @@
+"""
+Tool for generating TOMLs for parameter sweeps in fenics_ice
+
+Input:
+------
+toml_name: the template/base config file
+target_param: the parameter to be swept (e.g. 'delta_beta')
+name_suff: acronym to identify this sweep in runnames etc
+           e.g. 'db' for delta_beta
+param_min, param_max: start and end of sweep values (inclusive!)
+command_template: this helps generate a bash script which will run each
+           simulation in turn.
+
+By default this sweeps orders of magnitude but this can be controlled by
+editing the call to np.geomspace.
+
+@authors: Fenics_ice contributors
+"""
+import sys
+import os
+import toml
+import numpy as np
+from pathlib import Path
+from functools import reduce
+import operator
+import argparse
+from configobj import ConfigObj
+from decimal import Decimal
+import copy
+import re
+from IPython import embed
+
+def getTomlItem(dictionary, key_list):
+    return reduce(operator.getitem, key_list, dictionary)
+
+def setTomlItem(dictionary, key_list, value):
+    param = reduce(operator.getitem, key_list[:-1], dictionary)
+    param[key_list[-1]] = value
+
+def composeName(root, suff, value):
+    assert isinstance(value, float)
+    value_str = "%1.0e" % value
+    return "_".join((root, suff, value_str))
+
+# Argument passer
+parser = argparse.ArgumentParser()
+parser.add_argument("-conf", type=str,
+                    default="../../../config.ini", help="pass config file")
+parser.add_argument("-path_toml_template", type=str,
+                    default="", help="pass toml template path")
+parser.add_argument("-target_param", type=str,
+                    default="delta_beta",
+                    help="the parameter to be swept e.g. delta_beta")
+parser.add_argument("-name_sweep", type=str,
+                    default="db",
+                    help="acronym to identify this sweep in run names"
+                         " etc e.g. db for delta_beta")
+parser.add_argument("-min", type=np.float64,
+                    default=1.0e2,
+                    help="minimum value for start sweep (inclusive)")
+parser.add_argument("-max", type=np.float64,
+                    default=1.0e10,
+                    help="max value for start sweep (inclusive)")
+
+args = parser.parse_args()
+
+# Get smith_glacier repo configuration file
+config_file = args.conf
+config = ConfigObj(os.path.expanduser(config_file))
+
+# Main directory path
+MAIN_PATH = config['main_path']
+sys.path.append(MAIN_PATH)
+
+# Get .toml file template
+toml_name = Path(args.path_toml_template)
+assert toml_name.exists(), f".toml file template {toml_name} not found"
+
+# target_param = ["inversion", "delta_beta"]
+target_param = args.target_param
+name_suff = args.name_sweep
+
+# Set parameter range
+param_min = args.min
+param_max = args.max
+
+runs_directory = os.path.join(MAIN_PATH, 'scripts/run_stages/run_lcurves')
+tomls_f = os.path.join(runs_directory, 'tomls_'+target_param)
+output_dir = os.path.join(MAIN_PATH, 'output/04_run_inv_lcurves')
+output_dir_run = os.path.join(output_dir, target_param)
+
+# Paths to data
+if not os.path.exists(tomls_f):
+    os.makedirs(tomls_f)
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+if not os.path.exists(output_dir_run):
+    os.makedirs(output_dir_run)
+
+command_template = "mpirun -n 12 python $RUN_CONFIG_DIR/run_lcurves/run_lcurves_inv.py $OUTPUT_DIR/04_lcurves/{toml_fname} |& tee {log_fname}\n"
+script_name = f"sweep_{target_param}.sh"
+
+name_param = ["io", "run_name"]
+# Regexes
+run_name_re = re.compile('^(run_name = \"*).*(\")')
+param_re = re.compile(rf"^({target_param} = ).*")
+out_dir_re = re.compile('^(output_dir = \"*).*(\")')
+
+
+# Compute round steps if possible
+steps = int(np.log10(param_max) - np.log10(param_min)) + 1
+
+# toml writes "np.float*" types as strings, so convert to list of floats here
+param_range = np.geomspace(param_min, param_max, steps, dtype=np.float64).tolist()
+print('will sweep over', param_range)
+
+template = toml.load(toml_name)
+template_name = getTomlItem(template, name_param)
+script_file = open(os.path.join(tomls_f,script_name), 'w')
+
+for i in range(steps):
+
+    runname = composeName(template_name, name_suff, param_range[i])
+    filename = Path(tomls_f,composeName(toml_name.stem, name_suff, param_range[i])).with_suffix(".toml")
+
+    with open(toml_name, 'r') as inny:
+        lines = inny.readlines()
+
+        with open(filename, 'w') as outy:
+            for line in lines:
+                if run_name_re.match(line):
+                    new_line = run_name_re.sub(f"\\1{runname}\\2", line)
+                    print(new_line)
+                elif param_re.match(line):
+                    #new_line = param_re.match(line).group(1) + "%e\n" % param_range[i]
+                    new_line = param_re.match(line).group(1) + "{:.1E}".format(Decimal(param_range[i])) + "\n"
+                    print(new_line)
+                elif out_dir_re.match(line):
+                    new_line = out_dir_re.sub(f"\\1{output_dir_run}\\2", line)
+                else:
+                    new_line = line
+                outy.write(new_line)
+
+    # append to shell script
+    cmd = command_template.replace("{toml_fname}", filename.name).\
+        replace("{log_fname}", f"log_{runname}")
+    script_file.write(cmd)
---
+++ scripts/run_stages/run_lcurves/run_lcurves_inv.py
@@ -0,0 +1,135 @@
+# For fenics_ice copyright information see ACKNOWLEDGEMENTS in the fenics_ice
+# root directory
+
+# This file is part of fenics_ice.
+#
+# fenics_ice is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# fenics_ice is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with tlm_adjoint.  If not, see <https://www.gnu.org/licenses/>.
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+
+import sys
+from pathlib import Path
+from dolfin import *
+from tlm_adjoint.fenics import *
+
+from fenics_ice import model, solver, inout
+from fenics_ice import mesh as fice_mesh
+from fenics_ice.config import ConfigParser
+# import fenics_ice.fenics_util as fu
+# import matplotlib as mpl
+# mpl.use("Agg")
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import time
+# import pickle
+import datetime
+
+
+def run_inv(config_file):
+    """Run the inversion part of the simulation"""
+    # Read run config file
+    params = ConfigParser(config_file)
+
+    inout.setup_logging(params)
+    inout.log_preamble("inverse", params)
+
+    # Load the static model data (geometry, smb, etc)
+    input_data = inout.InputData(params)
+
+    # Get the model mesh
+    mesh = fice_mesh.get_mesh(params)
+    mdl = model.model(mesh, input_data, params)
+
+    # pts_lengthscale = params.obs.pts_len
+
+    mdl.gen_alpha()
+
+    # Add random noise to Beta field iff we're inverting for it
+    mdl.bglen_from_data()
+    mdl.init_beta(mdl.bglen_to_beta(mdl.bglen), pert=False)
+
+    # Next line will output the initial guess for alpha fed into the inversion
+    # File(os.path.join(outdir,'alpha_initguess.pvd')) << mdl.alpha
+
+    #####################
+    # Run the Inversion #
+    #####################
+
+    slvr = solver.ssa_solver(mdl)
+    slvr.inversion()
+
+    ###########################
+    #  Write out variables    #
+    ###########################
+
+    outdir = params.io.output_dir
+
+    # Required for next phase (HDF5):
+
+    invout_file = params.io.inversion_file
+    invout = HDF5File(mesh.mpi_comm(), str(Path(outdir)/invout_file), 'w')
+
+    # invout.parameters.add("gamma_alpha", slvr.gamma_alpha)
+    # invout.parameters.add("delta_alpha", slvr.delta_alpha)
+    # invout.parameters.add("gamma_beta", slvr.gamma_beta)
+    # invout.parameters.add("delta_beta", slvr.delta_beta)
+    # invout.parameters.add("delta_beta_gnd", slvr.delta_beta_gnd)
+    # invout.parameters.add("timestamp", str(datetime.datetime.now()))
+    #
+    invout.write(mdl.alpha, 'alpha')
+    invout.write(mdl.beta, 'beta')
+
+    # For visualisation (XML & VTK):
+
+    inout.write_variable(slvr.U, params)
+    inout.write_variable(mdl.beta, params)
+
+    mdl.beta_bgd.rename("beta_bgd", "")
+    #inout.write_variable(mdl.beta_bgd, params)
+
+    #inout.write_variable(mdl.bed, params)
+    H = project(mdl.H, mdl.M)
+    H.rename("thick", "")
+    #inout.write_variable(H, params)
+
+    fl_ex = project(slvr.float_conditional(H), mdl.M)
+    #inout.write_variable(fl_ex, params, name='float')
+
+    # inout.write_variable(mdl.mask_vel_M, params, name="mask_vel")
+
+    # inout.write_variable(mdl.u_obs_Q, params)
+    # inout.write_variable(mdl.v_obs_Q, params)
+    # inout.write_variable(mdl.u_std_Q, params)
+    # inout.write_variable(mdl.v_std_Q, params)
+
+    U_obs = project((mdl.v_obs_Q**2 + mdl.u_obs_Q**2)**(1.0/2.0), mdl.M)
+    U_obs.rename("uv_obs", "")
+    #inout.write_variable(U_obs, params, name="uv_obs")
+
+    inout.write_variable(mdl.alpha, params)
+
+    Bglen = project(slvr.beta_to_bglen(slvr.beta), mdl.M)
+    Bglen.rename("Bglen", "")
+    inout.write_variable(Bglen, params)
+    #inout.write_variable(slvr.bmelt, params, name="bmelt")
+    #inout.write_variable(slvr.smb, params, name="smb")
+    #inout.write_variable(mdl.surf, params, name="surf")
+
+    return mdl
+
+
+if __name__ == "__main__":
+    stop_annotating()
+    assert len(sys.argv) == 2, "Expected a configuration file (*.toml)"
+    run_inv(sys.argv[1])

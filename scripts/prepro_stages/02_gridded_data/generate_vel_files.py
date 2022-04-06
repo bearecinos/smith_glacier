c8010312a935277e18338de092015a1e9fb4c724
bearecinos <beatriz.recinos@ed.ac.uk>
2022-04-06 12:40:41 +0100

    improved generate_vel_files

--- scripts/prepro_stages/02_gridded_data/generate_vel_files.py
+++ scripts/prepro_stages/02_gridded_data/generate_vel_files.py
@@ -1,6 +1,6 @@
 """
-Crops ASE 2010 velocities and different composite mosaic's from
- different velocity data products to an area extend defined for now
+Crops satellite velocities for individual years and composite means
+ from different velocity data products to an area extent defined (for now)
   for the smith glacier experiment.
 
 Options of data products for the composite velocity mosaic:
@@ -8,12 +8,12 @@ Options of data products for the composite velocity mosaic:
 - ITSLIVE
 
 Options for the cloud point velocity:
-- Only ASE 2010
-- ASE 2010 plus extra data from ITSlive 2011 to fill more nans
+- Measures without filling gaps 2013-2014
+- ITSLIVE without filling gaps 2011
 
 The code generates a .h5 file, with the corresponding velocity
 file suffix, depending on what has been chosen as data:
-e.g. `_itslive-comp_ase-itslive-cloud.h5`
+e.g. `_itslive-comp_cloud.h5`
 
 The file contain the following variables stored as tuples
 and containing no np.nan's:
@@ -40,11 +40,9 @@ parser.add_argument("-composite",
                     help="Data product for the composite velocities: itslive or measures")
 parser.add_argument("-add_cloud_data",
                     action="store_true",
-                    help="If specified we add ASE 2010 velocities "
-                         "as cloud point velocities to the .h5 file")
-parser.add_argument("-add_extra_data_to_cloud",
-                    action="store_true",
-                    help="If specified we add extra data to ASE cloud point velocities")
+                    help="If this is specify a year for the data is selected "
+                         "we dont interpolate nans and add the data as it is "
+                         " as cloud point velocities to the .h5 file")
 args = parser.parse_args()
 config_file = args.conf
 config = ConfigObj(os.path.expanduser(config_file))
@@ -109,7 +107,7 @@ if args.composite == 'itslive':
     vx_err.data[non_valid_e] = 0.0
 
     vx_s, x_s, y_s = vel_tools.crop_velocity_data_to_extend(vx, smith_bbox,
-                                                              return_coords=True)
+                                                            return_coords=True)
     vy_s = vel_tools.crop_velocity_data_to_extend(vy, smith_bbox)
     vx_err_s = vel_tools.crop_velocity_data_to_extend(vx_err, smith_bbox)
     vy_err_s = vel_tools.crop_velocity_data_to_extend(vy_err, smith_bbox)
@@ -132,12 +130,74 @@ if args.composite == 'itslive':
 
     errx_comp = vx_err_s.ravel()
     erry_comp = vy_err_s.ravel()
+
+    x_cloud = None
+    y_cloud = None
+    vx_cloud = None
+    vy_cloud = None
+    vx_err_cloud = None
+    vy_err_cloud = None
+
+    if args.add_cloud_data:
+        print('The velocity product for the cloud '
+              'point data its ITSlive 2010')
+        # Opening files with salem slower than rasterio
+        # but they end up more organised in xr.DataArrays
+        dvx = salem.open_xr_dataset(paths_itslive[4])
+        dvx_err = salem.open_xr_dataset(paths_itslive[5])
+        dvy = salem.open_xr_dataset(paths_itslive[6])
+        dvy_err = salem.open_xr_dataset(paths_itslive[7])
+
+        vx = dvx.data
+        vy = dvy.data
+        vx_err = dvx_err.data
+        vy_err = dvy_err.data
+
+        nodata = -32767.0
+
+        non_valid = (vx == nodata) | (vy == nodata)
+        non_valid_e = (vx_err == nodata) | (vy_err == nodata)
+
+        # We set invalid data (nans) to zero
+        # After plotting the data we saw that nan's
+        # were only found in the ocean where fenics_ice
+        # does nothing...
+        vx.data[non_valid] = 0.0
+        vy.data[non_valid] = 0.0
+        vx_err.data[non_valid_e] = 0.0
+        vx_err.data[non_valid_e] = 0.0
+
+        vx_s, x_s, y_s = vel_tools.crop_velocity_data_to_extend(vx, smith_bbox,
+                                                                return_coords=True)
+        vy_s = vel_tools.crop_velocity_data_to_extend(vy, smith_bbox)
+        vx_err_s = vel_tools.crop_velocity_data_to_extend(vx_err, smith_bbox)
+        vy_err_s = vel_tools.crop_velocity_data_to_extend(vy_err, smith_bbox)
+
+        print('Lets check all has the same shape')
+        print(vx_s.shape, vy_s.shape)
+        print(vx_err_s.shape, vy_err_s.shape)
+        print(y_s.shape)
+        print(x_s.shape)
+
+        x_grid, y_grid = np.meshgrid(x_s, y_s)
+
+        # Ravel all arrays so they can be stored with
+        # a tuple shape (values, )
+        x_cloud = x_grid.ravel()
+        y_cloud = y_grid.ravel()
+
+        vx_cloud = vx_s.ravel()
+        vy_cloud = vy_s.ravel()
+
+        vx_err_cloud = vx_err_s.ravel()
+        vy_err_cloud = vy_err_s.ravel()
+
 else:
     print('The velocity product for the composite solution will be MEaSUREs')
 
     # First load and process MEaSUREs data for storing a composite mean of
     # all velocity components and uncertainty
-    path_measures = os.path.join(MAIN_PATH, config['measures'])
+    path_measures = os.path.join(MAIN_PATH, config['measures_comp'])
 
     dm = xr.open_dataset(path_measures)
 
@@ -148,184 +208,72 @@ else:
 
     # Crop velocity data to the Smith Glacier extend
     vx_s, x_s, y_s = vel_tools.crop_velocity_data_to_extend(vx, smith_bbox,
-                                                             return_coords=True)
+                                                            return_coords=True)
     vy_s = vel_tools.crop_velocity_data_to_extend(vy, smith_bbox)
     std_vx_s = vel_tools.crop_velocity_data_to_extend(std_vx, smith_bbox)
     std_vy_s = vel_tools.crop_velocity_data_to_extend(std_vy, smith_bbox)
 
     # Mask arrays and interpolate nan with nearest neighbor
     x_grid, y_grid = np.meshgrid(x_s, y_s)
-    # vx_int = vel_tools.interpolate_missing_data(vx_s, x_grid, y_grid)
-    # vy_int = vel_tools.interpolate_missing_data(vy_s, x_grid, y_grid)
-    # stdvx_int = vel_tools.interpolate_missing_data(std_vx_s, x_grid, y_grid)
-    # stdvy_int = vel_tools.interpolate_missing_data(std_vy_s, x_grid, y_grid)
+    vx_int = vel_tools.interpolate_missing_data(vx_s, x_grid, y_grid)
+    vy_int = vel_tools.interpolate_missing_data(vy_s, x_grid, y_grid)
+    stdvx_int = vel_tools.interpolate_missing_data(std_vx_s, x_grid, y_grid)
+    stdvy_int = vel_tools.interpolate_missing_data(std_vy_s, x_grid, y_grid)
 
     # Ravel all arrays so they can be stored with
     # a tuple shape (values, )
     x_comp = x_grid.ravel()
     y_comp = y_grid.ravel()
-    vx_comp = vx_s.ravel()
-    vy_comp = vy_s.ravel()
-    errx_comp = std_vx_s.ravel()
-    erry_comp = std_vy_s.ravel()
+    vx_comp = vx_int
+    vy_comp = vy_int
+    errx_comp = stdvx_int
+    erry_comp = stdvy_int
 
-x_cloud = None
-y_cloud = None
-vx_cloud = None
-vy_cloud = None
-vx_err_cloud = None
-vy_err_cloud = None
-vel_obs_cloud = None
+    x_cloud = None
+    y_cloud = None
+    vx_cloud = None
+    vy_cloud = None
+    vx_err_cloud = None
+    vy_err_cloud = None
 
-if args.add_cloud_data:
-    #2) Now lets focus on cloud data, we load the ASE Time Series - Ice Velocity 450 m
-    # resolution for point could velocity observations.
-    dase = xr.open_dataset(os.path.join(MAIN_PATH,
-                                        config['velocity_ase_series']))
+    if args.add_cloud_data:
+        print('The velocity product for the cloud '
+              'point data its Measures 2013-2014')
 
-    dase = dase.rename_dims({'ny': 'y','nx': 'x'})
+        path_measures = os.path.join(MAIN_PATH, config['measures_cloud'])
 
-    # We take everything in 2010 as is the year close to BedMachine date
-    year = '2010'
-    list_vars = ['vx', 'vy', 'err']
+        dm = xr.open_dataset(path_measures)
 
-    keys = []
-    for var in list_vars:
-        keys.append(f'{var}{year}')
+        vx = dm.VX
+        vy = dm.VY
+        std_vx = dm.STDX
+        std_vy = dm.STDY
 
-    vx_ase = dase[keys[0]]
-    vy_ase = dase[keys[1]]
-    err_ase = dase[keys[2]]
+        # Crop velocity data to the Smith Glacier extend
+        vx_s, x_s, y_s = vel_tools.crop_velocity_data_to_extend(vx, smith_bbox,
+                                                                return_coords=True)
+        vy_s = vel_tools.crop_velocity_data_to_extend(vy, smith_bbox)
+        std_vx_s = vel_tools.crop_velocity_data_to_extend(std_vx, smith_bbox)
+        std_vy_s = vel_tools.crop_velocity_data_to_extend(std_vy, smith_bbox)
 
-    x_ase = dase['xaxis']
-    y_ase = dase['yaxis']
+        # Mask arrays and interpolate nan with nearest neighbor
+        x_grid, y_grid = np.meshgrid(x_s, y_s)
 
-    # Calculating vector magnitude
-    vel_ase_obs = np.sqrt(vx_ase**2 + vy_ase**2).rename('vel_obs')
-
-    # Now we need the error components of the error magnitude vector
-    # To find them I am assuming the dir is the same as the velocity vector
-    vel_err_dir = np.arctan(vy_ase/vx_ase) #* 180/np.pi not sure about the angle
-
-    # err-y component
-    err_y = err_ase*np.cos(vel_err_dir).rename('err_y')
-    # err-x component
-    err_x = err_ase*np.sin(vel_err_dir).rename('err_y')
-
-    err_obs_check = np.sqrt(err_x**2 + err_y**2)
-    err_obs_check = err_obs_check.rename('err2010')
-
-    # Assigned coordinates to xarrays
-    vx_ase = vx_ase.assign_coords({"x": x_ase, "y": y_ase})
-    vy_ase = vy_ase.assign_coords({"x": x_ase, "y": y_ase})
-    err_x = err_x.assign_coords({"x": x_ase, "y": y_ase})
-    err_y = err_y.assign_coords({"x": x_ase, "y": y_ase})
-
-    vel_ase_obs = vel_ase_obs.assign_coords({"x": x_ase, "y": y_ase})
-
-    # Crop velocity data to the Smith Glacier extend
-    vx_slice, xind_ase, yind_ase = vel_tools.crop_velocity_data_to_extend(vx_ase,
-                                                                          smith_bbox,
-                                                                          return_xarray=True,
-                                                                          return_indexes=True)
-    vy_slice = vel_tools.crop_velocity_data_to_extend(vy_ase, smith_bbox, return_xarray=True)
-    err_y_slice = vel_tools.crop_velocity_data_to_extend(err_y, smith_bbox, return_xarray=True)
-    err_x_slice = vel_tools.crop_velocity_data_to_extend(err_x, smith_bbox, return_xarray=True)
-
-    x_slice = x_ase[xind_ase]
-    y_slice = y_ase[yind_ase]
-
-    vel_obs_slice = vel_tools.crop_velocity_data_to_extend(vel_ase_obs, smith_bbox,
-                                                           return_xarray=True)
-
-    # 3) We get rid of outliers in the data
-    y_out, x_out = np.where(vel_obs_slice.data > 5000)
-    vel_obs_slice[y_out, x_out] = np.nan
-    vx_slice[y_out, x_out] = np.nan
-    vy_slice[y_out, x_out] = np.nan
-    err_y_slice[y_out, x_out] = np.nan
-    err_x_slice[y_out, x_out] = np.nan
-
-    shape_before = vy_slice.shape
-    print('Shape before nan drop')
-    print(shape_before)
-
-    if args.add_extra_data_to_cloud:
-        print('We will add extra data from ITSLIVE 2011 '
-              'to ASE 2010 to cover up more nans')
-
-        path_itslive_2011 = os.path.join(MAIN_PATH,
-                                 config['itslive_2011'])
-
-        output_itslive = vel_tools.open_and_crop_itslive_data(path_itslive_2011,
-                                                              extend=smith_bbox,
-                                                              x_to_int=x_slice,
-                                                              y_to_int=y_slice)
-
-        vel_live = output_itslive[0].data
-        vx_live = output_itslive[1].data
-        vy_live = output_itslive[2].data
-        vxerr_live = output_itslive[3].data
-        vyerr_live = output_itslive[4].data
-
-        # We add non-nan values from itslive to ASE numpy arrays
-        mask_nan = np.isnan(vel_obs_slice)
-
-        print('Number of nans before adding new data')
-        print(np.count_nonzero(mask_nan))
-
-        vel_final_ase = vel_obs_slice.data
-        vel_final_ase[mask_nan] = vel_live[mask_nan]
-
-        vxf_ase = vx_slice.data
-        vxf_ase[mask_nan] = vx_live[mask_nan]
-
-        vyf_ase = vy_slice.data
-        vyf_ase[mask_nan] = vy_live[mask_nan]
-
-        errxf_ase = err_x_slice.data
-        errxf_ase[mask_nan] = vxerr_live[mask_nan]
-
-        erryf_ase = err_y_slice.data
-        erryf_ase[mask_nan] = vyerr_live[mask_nan]
-    else:
-        print('We add nothing new to ASE')
-        vel_final_ase = vel_obs_slice.data
-        vxf_ase = vx_slice.data
-        vyf_ase = vy_slice.data
-        errxf_ase = err_x_slice.data
-        erryf_ase = err_y_slice.data
-
-    ## 4) We get rid of nan data for all the cloud arrays
-    vel_ma = np.ma.masked_invalid(vel_final_ase)
-    print('Number of nans after adding new data')
-    print(np.count_nonzero(vel_ma.mask))
-
-    output_cloud = vel_tools.drop_invalid_data_from_several_arrays(x_slice,
-                                                                   y_slice,
-                                                                   vxf_ase,
-                                                                   vyf_ase,
-                                                                   errxf_ase,
-                                                                   erryf_ase,
-                                                                   vel_ma)
-
-    x_cloud = output_cloud[0]
-    y_cloud = output_cloud[1]
-    vx_cloud = output_cloud[2]
-    vy_cloud = output_cloud[3]
-    vx_err_cloud = output_cloud[4]
-    vy_err_cloud = output_cloud[5]
-    vel_obs_cloud = output_cloud[6]
+        # Ravel all arrays so they can be stored with
+        # a tuple shape (values, )
+        x_cloud = x_grid.ravel()
+        y_cloud = y_grid.ravel()
+        vx_cloud = vx_s.ravel()
+        vy_cloud = vy_s.ravel()
+        vx_err_cloud = std_vx_s.ravel()
+        vy_err_cloud = std_vy_s.ravel()
 
 mask_comp = np.array(vx_comp, dtype=bool)
-
+print(mask_comp.shape)
 
 composite = args.composite + '-comp_'
 if args.add_cloud_data:
-    if args.add_extra_data_to_cloud:
-        file_suffix = composite + 'ase-istlive-cloud' + '.h5'
-    else:
-        file_suffix = composite + 'ase-cloud' + '.h5'
+    file_suffix = composite + 'cloud' + '.h5'
 else:
     file_suffix = args.composite + '-comp' + '.h5'
 

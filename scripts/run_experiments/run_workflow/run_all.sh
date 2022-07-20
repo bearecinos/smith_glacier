db7ff6f5574606f658ae02869ceac81cb3424b6c
bearecinos <beatriz.recinos@ed.ac.uk>
2022-07-20 12:21:34 +0100

    experiments original data sets with new priors

--- scripts/run_experiments/run_workflow/run_all.sh
+++ scripts/run_experiments/run_workflow/run_all.sh
@@ -12,13 +12,13 @@ fi
 
 echo "Copying prepro output files to input run dir"
 # Mesh files
-cp $OUTPUT_DIR/01_mesh/smith_variable_ocean* $input_run_inv/.
+#cp $OUTPUT_DIR/01_mesh/smith_variable_ocean* $input_run_inv/.
 # Gridded data files
-cp $OUTPUT_DIR/02_gridded_data/smith_bedmachine.h5 $input_run_inv/.
-cp $OUTPUT_DIR/02_gridded_data/smith_smb.h5 $input_run_inv/.
-cp $OUTPUT_DIR/02_gridded_data/smith_bglen.h5 $input_run_inv/.
-cp $OUTPUT_DIR/02_gridded_data/smith_obs_vel_* $input_run_inv/.
-cp $OUTPUT_DIR/02_gridded_data/smith_melt_depth_params.h5 $input_run_inv/.
+#cp $OUTPUT_DIR/02_gridded_data/smith_bedmachine.h5 $input_run_inv/.
+#cp $OUTPUT_DIR/02_gridded_data/smith_smb.h5 $input_run_inv/.
+#cp $OUTPUT_DIR/02_gridded_data/smith_bglen.h5 $input_run_inv/.
+#cp $OUTPUT_DIR/02_gridded_data/smith_obs_vel_* $input_run_inv/.
+#cp $OUTPUT_DIR/02_gridded_data/smith_melt_depth_params.h5 $input_run_inv/.
 
 # Create output directory for inversion output
 export run_inv_output_dir=$OUTPUT_DIR/03_run_inv
@@ -44,15 +44,15 @@ mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_forward.py $RUN_CONFIG_DIR/run
 
 echo $(date -u) "Done with Forward!"
 
-mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_eigendec.py $RUN_CONFIG_DIR/run_workflow/$2
+#mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_eigendec.py $RUN_CONFIG_DIR/run_workflow/$2
 
-echo $(date -u) "Done with eigendec -------------------------------------------------------!"
+#echo $(date -u) "Done with eigendec -------------------------------------------------------!"
 
-mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_errorprop.py $RUN_CONFIG_DIR/run_workflow/$2
+#mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_errorprop.py $RUN_CONFIG_DIR/run_workflow/$2
 
-echo $(date -u) "Done with error propagation"
+#echo $(date -u) "Done with error propagation"
 
-mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_invsigma.py $RUN_CONFIG_DIR/run_workflow/$2
+#mpirun -n $1 python $FENICS_ICE_BASE_DIR/runs/run_invsigma.py $RUN_CONFIG_DIR/run_workflow/$2
 
-echo $(date -u) "We are done with the whole workflow!"
+#echo $(date -u) "We are done with the whole workflow!"
 

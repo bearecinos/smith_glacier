cb84f27de613dd9655bb7de8e05d642917130319
bearecinos <beatriz.recinos@ed.ac.uk>
2022-09-12 12:09:13 +0100

    corrections for post processing of modeloutput

--- scripts/post_processing/re_grid_fenics_ice_output.py
+++ scripts/post_processing/re_grid_fenics_ice_output.py
@@ -1,7 +1,9 @@
 import sys
 import argparse
 import numpy as np
-from scipy.interpolate import LinearNDInterpolator
+from scipy.interpolate import griddata
+import h5py
+import numpy.ma as ma
 import salem
 import pyproj
 import xarray as xr
@@ -121,19 +123,85 @@ thick_v = thick_p.compute_vertex_values(mesh_in)
 x, y, t = graphics.read_fenics_ice_mesh(mesh_in)
 trim = tri.Triangulation(x, y, t)
 
-path_dan_grid = os.path.join(MAIN_PATH, 'scripts/prepro_stages/02_gridded_data/grid_for_bea.npy')
+path_dan_grid = os.path.join(MAIN_PATH, 'scripts/post_processing/grid_for_bea.npy')
 
 D = np.load(path_dan_grid)
 X = D[0]
 Y = D[1]
 
-points = np.vstack(([x],[y]))
+print('Only need to do the interpolations')
+uv_obs_int = griddata((x, y), uv_obs_v.ravel(),
+                   (X, Y),
+                   method='nearest', fill_value=np.nan)
 
-linTriFn = LinearNDInterpolator(points.T, alpha_v)
+alpha_int = griddata((x, y), alpha_v.ravel(),
+                   (X, Y),
+                   method='nearest', fill_value=np.nan)
 
-xi, yi = np.meshgrid(X, Y)
+beta_int = griddata((x, y), beta_v.ravel(),
+                   (X, Y),
+                   method='nearest', fill_value=np.nan)
 
-print('Only need to do interpolation')
-zi_lin = linTriFn(xi, yi)
+U_int = griddata((x, y), U_v.ravel(),
+                   (X, Y),
+                   method='nearest', fill_value=np.nan)
 
-#print(zi_lin.shape)
+bed_int = griddata((x, y), bed_v.ravel(),
+                   (X, Y),
+                   method='nearest', fill_value=np.nan)
+
+thick_int = griddata((x, y), thick_v.ravel(),
+                   (X, Y),
+                   method='nearest', fill_value=np.nan)
+
+assert uv_obs_int.shape == X.shape
+assert uv_obs_int.shape == Y.shape
+assert alpha_int.shape == X.shape
+assert alpha_int.shape == Y.shape
+assert beta_int.shape == X.shape
+assert beta_int.shape == Y.shape
+assert U_int.shape == X.shape
+assert U_int.shape == Y.shape
+assert bed_int.shape == X.shape
+assert bed_int.shape == Y.shape
+assert thick_int.shape == X.shape
+assert thick_int.shape == Y.shape
+
+## Get rid of interpolated values in the ocean using bedmachine mask
+input_dir = params.io.input_dir
+file_bm = Path(input_dir) / params.io.bed_data_file
+
+BM_file = h5py.File(file_bm, 'r')
+
+bm_x = BM_file['x'][:]
+bm_y = BM_file['y'][:]
+
+bmxx, bmyy = np.meshgrid(bm_x, bm_y)
+
+bm_surf = BM_file['surf'][:]
+
+bm_surf_int = griddata((bmxx.ravel(), bmyy.ravel()), bm_surf.ravel(),
+                       (X, Y), method='nearest', fill_value=np.nan)
+
+array_new = ma.masked_where(bm_surf_int == 0, bm_surf_int)
+
+uv_obs_int[array_new.mask] = np.nan
+alpha_int[array_new.mask] = np.nan
+beta_int[array_new.mask] = np.nan
+U_int[array_new.mask] = np.nan
+bed_int[array_new.mask] = np.nan
+thick_int[array_new.mask] = np.nan
+
+output_local_dir = os.path.join(MAIN_PATH, 'output/07_post_processing_model/'+ phase_suffix_il)
+if not os.path.exists(output_local_dir):
+    os.makedirs(output_local_dir)
+
+np.savez(os.path.join(output_local_dir, 'fenics_ice_output_gridded'),
+         X=X,
+         Y=Y,
+         vel_obs=uv_obs_int,
+         vel_model=U_int,
+         alpha=alpha_int,
+         beta=beta_int,
+         bed=bed_int,
+         thick=thick_int)
